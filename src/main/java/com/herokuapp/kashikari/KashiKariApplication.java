438abde0e9658a72c0f90800b96dfd1b9605f45a
tachikawaYutaka <tachikawa.yutaka.web@gmail.com>
2018-10-03 00:32:13 +0900

    develop init

---
+++ src/main/java/com/herokuapp/kashikari/KashiKariApplication.java
@@ -0,0 +1,12 @@
+package com.herokuapp.kashikari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KashiKariApplication  {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KashiKariApplication.class, args);
+    }
+}
