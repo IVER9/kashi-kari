37303b069b8a2c5d837c1a490c1a7f5a7737ab44
tachikawaYutaka <tachikawa.yutaka.web@gmail.com>
2018-10-16 00:42:05 +0900

    for heroku
    for heroku
    
    Added maven wrapper
    
    modify Procfile
    
    modify setting
    
    modify setting
    
    modify setting
    
    modify mapper
    
    modify mapper
    
    modify mapper
    
    modify mapper
    
    modify mapper
    
    modify mapper
    
    modify mapper
    
    modify mapper
    
    modify mapper
    
    modify mapper
    
    modify mapper
    
    modify mapper
    
    modify mapper
    
    modify mapper
    
    modify mapper
    
    modify mapper

--- src/main/resources/db/migration/V1.0.0__create_team_table.sql
+++ src/main/resources/db/migration/V1.0.0__create_team_table.sql
@@ -2,8 +2,8 @@ CREATE TABLE `team` (
   `id` varchar(255) NOT NULL,
   `url` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` datetime NOT NULL DEFAULT '1970-01-01 00:00:00',
+  `updated_at` datetime NOT NULL DEFAULT '1970-01-01 00:00:00',
   `deleted_flag` char(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
--- src/main/resources/db/migration/V1.1.0__create_member_table.sql
+++ src/main/resources/db/migration/V1.1.0__create_member_table.sql
@@ -3,8 +3,8 @@ CREATE TABLE `member` (
   `team_id` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
   `money` bigint(11) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` datetime NOT NULL DEFAULT '1970-01-01 00:00:00',
+  `updated_at` datetime NOT NULL DEFAULT '1970-01-01 00:00:00',
   `deleted_flag` char(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
